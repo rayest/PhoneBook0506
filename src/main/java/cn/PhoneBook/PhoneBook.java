567741cc75c91c2511bb7b96649aeb6f6cfcd9c5
Rayest <yi@rayest.cn>
2016-05-11 17:43:37 +0800

    Add Class DBUtil and ValidationUtil

--- src/main/java/cn/PhoneBook/PhoneBook.java
+++ src/main/java/cn/PhoneBook/PhoneBook.java
@@ -1,7 +1,7 @@
+
 package cn.PhoneBook;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,79 +9,27 @@ import java.util.List;
  */
 public class PhoneBook {
 
-    private List<Person> persons = new ArrayList<Person>();
-
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        String url = "jdbc:mysql://localhost/db?useUnicode=true" +
-                "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=true";
-        String user = "root";
-        String password = "199011081108";
-        return DriverManager.getConnection(url, user, password);
-    }
-
     public void addPerson(Person person) throws Exception {
-        Connection connection = getConnection();
-        String sql;
-        Statement statement = connection.createStatement();
-        sql = "INSERT persons(name,phoneNumber) VALUES ('" + person.getName() + "','" + person.getPhoneNumber() + "')";
-        statement.executeUpdate(sql);
+       DBUtil.addPerson(person);
     }
 
-    public List<Person> loadPersons() throws SQLException, ClassNotFoundException {
-        Connection connection = getConnection();
-        String sql;
-        Statement statement = connection.createStatement();
-        sql = "SELECT * FROM persons";
-        ResultSet resultSet = statement.executeQuery(sql);
-        List<Person> persons = new ArrayList<Person>();
-        while (resultSet.next()) {
-            Person person = new Person();
-            person.setName(resultSet.getString(1));
-            person.setPhoneNumber(resultSet.getString(2));
-            persons.add(person);
-        }
+
+    public List<Person> loadPersons(List<Person> persons) throws SQLException, ClassNotFoundException {
+        DBUtil.loadPersons(persons);
         return persons;
     }
 
-    public Person findPersonByName(String name) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
-        Statement statement = connection.createStatement();
-        String sql;
-        sql = "SELECT * FROM persons WHERE name ='" + name + "'";
-        ResultSet resultSet = statement.executeQuery(sql);
-        Person person = new Person();
 
-        while (resultSet.next()) {
-            person.setName(resultSet.getString(1));
-            person.setPhoneNumber(resultSet.getString(2));
-            persons.add(person);
-        }
-        return person;
+    public Person findPersonByName(String name) throws ClassNotFoundException, SQLException {
+        return DBUtil.findPersonByName(name);
     }
 
     public Person editPerson(String name, Person person) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
-        Statement statement = connection.createStatement();
-        String sql;
-        sql = "UPDATE persons SET name='" + person.getName() + "',phonenumber='" + person.getPhoneNumber() + "'WHERE name='" + name + "'";
-        statement.executeUpdate(sql);
+        DBUtil.editPerson(name,person);
         return person;
     }
 
     public void deletePerson(String name) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
-        Statement statement = connection.createStatement();
-        String sql;
-        sql = "DELETE FROM persons WHERE name='" + name + "'";
-        statement.executeUpdate(sql);
+        DBUtil.deletePerson(name);
     }
 }
-
-
-
-
-
-
-
